32f03a9fd4f58601d4bc631001b10d1f9f3de5ab
zappurenfuro <zarenfro23@gmail.com>
2024-06-15 10:44:31 +0700

    v1.0

---
+++ poefacron.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000; // Use the PORT environment variable provided by Render
+
+app.get('/', (req, res) => {
+  res.send('Cron job is running!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+const visitWebsite = async () => {
+  try {
+    const response = await axios.get('https://albionbalanceassistance-1.onrender.com/');
+    console.log(`Visited website. Status Code: ${response.status}`);
+  } catch (error) {
+    console.error('Error visiting website:', error.message);
+  }
+};
+
+// Schedule to run every 3 minutes
+setInterval(visitWebsite, 180000);
